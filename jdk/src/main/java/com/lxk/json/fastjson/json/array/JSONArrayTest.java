fbc870925b1c970ad59e2ec2b50cfe0ea1df382f
李学凯 <cmshome@163.com>
2024-05-14 14:32:09 +0800

    json array test

--- jdk/src/main/java/com/lxk/json/fastjson/json/array/JSONArrayTest.java
+++ jdk/src/main/java/com/lxk/json/fastjson/json/array/JSONArrayTest.java
@@ -1,10 +1,22 @@
 package com.lxk.json.fastjson.json.array;
 
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
 /**
  * JSONArray test
  *
  * @author LiXuekai on 2019/9/24
  */
 public class JSONArrayTest {
+
+
+    @Test
+    public void cast() {
+        String s = "[a,d,f,g,r,h,t]";
+
+        String s1 = s.substring(1, s.length()-1);
+        System.out.println(Lists.newArrayList(s1.split(",")));
+    }
 
 }
