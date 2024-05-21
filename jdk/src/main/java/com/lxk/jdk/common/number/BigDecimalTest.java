60fb011d2a876d7d80d6260dcb6ad57e92b10c2b
李学凯 <cmshome@163.com>
2024-05-21 15:18:35 +0800

    BigDecimal转字符串为Long是性能差点。

--- jdk/src/main/java/com/lxk/jdk/common/number/BigDecimalTest.java
+++ jdk/src/main/java/com/lxk/jdk/common/number/BigDecimalTest.java
@@ -1,8 +1,10 @@
 package com.lxk.jdk.common.number;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author LiXuekai on 2020/8/17
@@ -20,5 +22,40 @@ public class BigDecimalTest {
     public void lxk() {
         String num = "6.21E+18";
         System.out.println(new BigDecimal(num).toPlainString());
+    }
+
+
+    @Test
+    public void compare() {
+        List<String> list = Lists.newArrayList(
+                "2595859267661660161",
+                "2595859267661660161",
+                "3166439535",
+                "3166439515",
+                "3166439519",
+                "3166439518",
+                "3166439517",
+                "3166439516",
+                "3166439511"
+        );
+
+        while (true) {
+            // 56%
+            a(list);
+            // 44%
+            b(list);
+        }
+    }
+
+    private void b(List<String> list) {
+        for (String value : list) {
+            long b = Long.parseLong(value.trim());
+        }
+    }
+
+    private void a(List<String> list) {
+        for (String value : list) {
+            long a = new BigDecimal(value.trim()).longValue();
+        }
     }
 }
