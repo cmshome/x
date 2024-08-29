96078a1c6d6492a4ae1a16f2ded343bb5050594b
李学凯 <cmshome@163.com>
2024-08-29 16:21:02 +0800

    NS Pro

--- jdk/src/main/java/com/lxk/other/NintendoTest.java
+++ jdk/src/main/java/com/lxk/other/NintendoTest.java
@@ -15,13 +15,13 @@ public class NintendoTest {
     @Test
     public void show() {
         List<SwitchParts> parts = Lists.newArrayList();
-        parts.addAll(card());
+        parts.addAll(game());
         parts.addAll(hardware());
         int sum = parts.stream().mapToInt(SwitchParts::getValue).sum();
         System.out.println("消费了：" + sum + " ¥ ");
     }
 
-    private List<SwitchParts> card() {
+    private List<SwitchParts> game() {
         List<SwitchParts> list = Lists.newArrayList();
         list.add(new SwitchParts("塞尔达传说-旷野之息", LocalDate.of(2024, 8, 5), 234));
         list.add(new SwitchParts("健身环大冒险", LocalDate.of(2024, 7, 20), 334));
@@ -38,6 +38,7 @@ public class NintendoTest {
 
     private List<SwitchParts> hardware() {
         List<SwitchParts> list = Lists.newArrayList();
+        list.add(new SwitchParts("Switch NS PRO 手柄 塞尔达传说-王国之泪 全新", LocalDate.of(2024, 8, 29), 316));
         list.add(new SwitchParts("内存卡-64G", LocalDate.of(2024, 5, 18), 38));
         list.add(new SwitchParts("保护壳", LocalDate.of(2023, 12, 1), 25));
         list.add(new SwitchParts("贴膜", LocalDate.of(2023, 12, 1), 19));
