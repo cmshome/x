9ff69c9519816b9bd73a2432d8f8b64fdfd70e5c
李学凯 <cmshome@163.com>
2024-10-10 17:47:05 +0800

    max

--- jdk/src/main/java/com/lxk/guava/primitives/LongsTest.java
+++ jdk/src/main/java/com/lxk/guava/primitives/LongsTest.java
@@ -16,4 +16,16 @@ public class LongsTest {
         System.out.println(min);
         System.out.println(max);
     }
+
+
+    @Test
+    public void max() {
+        System.out.println("1058853744867524600");
+        // 9223372036854775807
+        System.out.println(Long.MAX_VALUE);
+        // 2147483647
+        System.out.println(Integer.MAX_VALUE);
+        // 1.7976931348623157E308
+        System.out.println(Double.MAX_VALUE);
+    }
 }
