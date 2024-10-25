81093a2a7a3f2e0e83b09b4a13c426955d3d8105
李学凯 <cmshome@163.com>
2024-10-25 13:40:04 +0800

    Vector线程安全，一个在写，另一个读就只得卡着。

---
+++ jdk/src/main/java/com/lxk/jdk/collection/TestVector.java
@@ -0,0 +1,74 @@
+package com.lxk.jdk.collection;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author LiXuekai on 2024/10/25
+ */
+public class TestVector {
+
+
+    @Test
+    public void safe() throws InterruptedException {
+        System.out.println("run");
+        ExecutorService pool = Executors.newFixedThreadPool(12);
+
+        List<String> list = new Vector<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("a" + i);
+        }
+        System.out.println(list.size());
+        Writer writer = new Writer(list);
+        Reader reader = new Reader(list);
+        pool.submit(writer);
+        pool.submit(reader);
+        TimeUnit.MINUTES.sleep(10);
+
+    }
+
+
+    public static class Writer implements Runnable {
+
+        private List<String> list;
+
+        public Writer(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Writer run.");
+
+            for (int i = 0; i < 20000; i++) {
+                list.add("b" + i);
+            }
+
+        }
+    }
+
+    public static class Reader implements Runnable {
+        private List<String> list;
+
+        public Reader(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Reader run.");
+            // 直接用Vector循环，因为线程安全，这个的一直等，知道写线程释放了，这地方才能读。
+            ArrayList<String> temp = Lists.newArrayList(list);
+            for (String s : temp) {
+                System.out.println("reader temp " + s);
+            }
+        }
+    }
+}
