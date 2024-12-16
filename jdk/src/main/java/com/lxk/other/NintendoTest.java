044d955c4477c8069549d1adaeb9b7b2ce5b5605
李学凯 <cmshome@163.com>
2024-12-16 17:08:47 +0800

    odyssey

--- jdk/src/main/java/com/lxk/other/NintendoTest.java
+++ jdk/src/main/java/com/lxk/other/NintendoTest.java
@@ -23,6 +23,7 @@ public class NintendoTest {
 
     private List<SwitchParts> game() {
         List<SwitchParts> list = Lists.newArrayList();
+        list.add(new SwitchParts("超级马里奥-奥德赛", LocalDate.of(2024, 11, 20), 225));
         list.add(new SwitchParts("女神异闻录5 皇家版 P5R", LocalDate.of(2024, 10, 22), 211));
         list.add(new SwitchParts("塞尔达无双-灾厄启示录", LocalDate.of(2024, 10, 22), 242));
         list.add(new SwitchParts("塞尔达传说-旷野之息", LocalDate.of(2024, 8, 5), 234));
@@ -40,6 +41,7 @@ public class NintendoTest {
 
     private List<SwitchParts> hardware() {
         List<SwitchParts> list = Lists.newArrayList();
+        list.add(new SwitchParts("底座原装充电器", LocalDate.of(2024, 12, 15), 69));
         list.add(new SwitchParts("底座盒透明保护罩", LocalDate.of(2024, 10, 28), 10));
         list.add(new SwitchParts("Switch NS PRO 手柄 塞尔达传说-王国之泪 全新", LocalDate.of(2024, 8, 29), 316));
         list.add(new SwitchParts("内存卡-64G", LocalDate.of(2024, 5, 18), 38));
