1da42c554b1ecd4c3abb0958b3b5f42be472ed24
李学凯 <cmshome@163.com>
2025-01-02 18:12:32 +0800

    TimeFormatTest

---
+++ jdk/src/main/java/com/lxk/jdk/common/TimeFormatTest.java
@@ -0,0 +1,22 @@
+package com.lxk.jdk.common;
+
+import com.lxk.tool.util.TimeUtils;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author LiXuekai on 2025/1/2
+ */
+public class TimeFormatTest {
+
+    @Test
+    public void formatZ() {
+        String s = "2025-01-02T03:17:43.353Z";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        LocalDateTime parse = LocalDateTime.parse(s, formatter);
+        long l = TimeUtils.toS(parse);
+        System.out.println(l);
+    }
+}
