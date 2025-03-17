a3bf69f116dd459488b8126a56ced625bf59c710
李学凯 <cmshome@163.com>
2025-03-17 13:48:34 +0800

    sum three

--- jdk/src/main/java/com/lxk/jdk/file/TestHuaBei.java
+++ jdk/src/main/java/com/lxk/jdk/file/TestHuaBei.java
@@ -39,14 +39,24 @@ public class TestHuaBei {
         System.out.println(fail);
     }
 
-    @Test
-    public void sum() {
-        List<Number> list = Lists.newArrayList(15, 32, 3, 19, 42, 15, 32, 47.5, 11.9, 12.9, 32.05, 0.3, 7.2, 5.9, 28, 2.7, 0.07, 19.37, 17.77, 24.67, 17.63, 47, 15, 3.35, 36, 15, 28.9, 36, 32, 54.5, 2.7, 110.2, 12.9, 25.9, 10.7, 97.9, 2.37, 3.9, 29, 11.82, 2.7, 17.9, 22.4, 21, 62.01, 34, 19, 15, 17.9, 27.9, 12.8, 19, 31, 15, 2.7, 10.9, 9.82, 2.7, 17, 32, 13, 16.77, 219, 15.02, 17, 31, 1, 1.09, 15, 32, 18, 19, 14.59, 13.45, 32, 15, 2.7, 10, 5, 2, 1.93, 83, 1.9, 0.01, 17.9, 0.17, 2.7, 15, 13.33, 0.12);
+    private void sum(List<Number> list) {
         list.sort(Comparator.comparingDouble(Number::doubleValue));
         Collections.reverse(list);
         System.out.println(list);
 
         double sum = list.stream().mapToDouble(Number::doubleValue).sum();
         System.out.println(sum);
+    }
+
+    @Test
+    public void three() {
+        List<Number> list = Lists.newArrayList(33, 2.53, 2.7, 15, 32, 3, 19, 42, 15, 32, 47.5, 11.9, 12.9, 32.05, 0.3, 7.2, 5.9, 28, 2.7, 0.07, 19.37, 17.77, 24.67, 17.63, 47, 15, 3.35, 36, 15, 28.9, 36, 32, 54.5, 2.7, 110.2, 12.9, 25.9, 10.7, 97.9, 2.37, 3.9, 29, 11.82, 2.7, 17.9, 22.4, 21, 62.01, 34, 19, 15, 17.9, 27.9, 12.8, 19, 31, 15, 2.7, 10.9, 9.82, 2.7, 17, 32, 13, 16.77, 219, 15.02, 17, 31, 1, 1.09, 15, 32, 18, 19, 14.59, 13.45, 32, 15, 2.7, 10, 5, 2, 1.93, 83, 1.9, 0.01, 17.9, 0.17, 2.7, 15, 13.33, 0.12);
+        // 2020.74
+        sum(list);
+    }
+
+    @Test
+    public void four() {
+        List<Number> list = Lists.newArrayList();
     }
 }
