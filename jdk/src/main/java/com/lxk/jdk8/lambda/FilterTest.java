a6db792be34b2d4d61477146ed16b51fce357c7b
李学凯 <cmshome@163.com>
2025-05-29 18:16:47 +0800

    lambda 先对value减1，再过滤value>0的数据

--- jdk/src/main/java/com/lxk/jdk8/lambda/FilterTest.java
+++ jdk/src/main/java/com/lxk/jdk8/lambda/FilterTest.java
@@ -3,9 +3,7 @@ package com.lxk.jdk8.lambda;
 import com.lxk.tool.util.CollectionUtil;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -31,5 +29,73 @@ public class FilterTest {
         System.out.println(list.size());
         List<String> collect = list.stream().filter(Objects::nonNull).collect(Collectors.toList());
         System.out.println(collect.size());
+    }
+
+    @Test
+    public void filterTreeMap() {
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("A", 1);
+        treeMap.put("B", 2);
+        treeMap.put("C", 3);
+        treeMap.put("D", 4);
+
+        // 使用lambda过滤掉值小于3的条目
+        Map<String, Integer> filteredMap = treeMap.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() >= 3)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        TreeMap::new
+                ));
+
+        System.out.println(filteredMap);
+    }
+
+
+    @Test
+    public void filterTreeMap2() {
+        TreeMap<String, Integer> originalMap = new TreeMap<>();
+        originalMap.put("A", 5);
+        originalMap.put("B", 3);
+        originalMap.put("C", 8);
+
+        TreeMap<String, Integer> modifiedMap = originalMap.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue() - 1,
+                        (oldValue, newValue) -> oldValue,
+                        TreeMap::new
+                ));
+
+        System.out.println("原始Map: " + originalMap);
+        System.out.println("修改后Map: " + modifiedMap);
+    }
+
+    @Test
+    public void filterTreeMap3() {
+        TreeMap<String, Integer> originalMap = new TreeMap<>();
+        originalMap.put("A", 5);
+        originalMap.put("B", 3);
+        originalMap.put("C", 8);
+        originalMap.put("D", 1);
+
+        // 先减1再过滤值大于0的条目
+        TreeMap<String, Integer> resultMap = originalMap.entrySet()
+                .stream()
+                .map(entry -> new AbstractMap.SimpleEntry<>(
+                        entry.getKey(),
+                        entry.getValue() - 1))
+                .filter(entry -> entry.getValue() > 0)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        TreeMap::new
+                ));
+
+        System.out.println("处理后结果: " + resultMap);
     }
 }
